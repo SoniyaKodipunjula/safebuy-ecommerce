516c5aec95743817a1afe336f96865e522890f88
SoniyaKodipunjula <sonny7sana@gmail.com>
2025-04-22 01:29:43 -0500

    Fix: Removed localStorage from server model to avoid runtime error

--- server/models/Otp.js
+++ server/models/Otp.js
@@ -6,6 +6,4 @@ const otpSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now, expires: 300 }, // 5 min expiry
 });
 
-localStorage.setItem("token", res.data.token);
-
 module.exports = mongoose.model('Otp', otpSchema);
